ba4ac91d8c5bbf0d6445d017e2f7c2b0559446ab
cjbischoff <christopherjbischoff@gmail.com>
2026-07-23 17:01:11 -0600

    feat(todo): add empty todo list page

--- 01-todo/todos/tests.py
+++ 01-todo/todos/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TodoListTests(TestCase):
+    """Tests for the TODO list page."""
+
+    def test_home_page_shows_empty_list(self):
+        """The home page explains when no TODOs exist."""
+        response = self.client.get("/")
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No TODOs yet.")
--- 01-todo/todos/views.py
+++ 01-todo/todos/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    """Display the TODO list."""
+    return render(request, "todos/home.html")
